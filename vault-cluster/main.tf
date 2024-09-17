55cb95f1bfff7cf4652d363148968892e9a07a7d
vagrant <vagrant@hellocloud-native-box>
2024-09-17 11:42:06 +0000

    added output.tf file

---
+++ vault-cluster/main.tf
@@ -0,0 +1,13 @@
+resource "hcp_hvn" "vault_hvn" {
+  hvn_id         = "zyt-cohort7"
+  cloud_provider = "aws"
+  region         = "ap-southeast-1"
+  cidr_block     = "172.25.16.0/20"
+}
+
+resource "hcp_vault_cluster" "vault_cluster" {
+  cluster_id = "cohort7-vault-cluster"
+  hvn_id     = hcp_hvn.example.hvn_id
+  tier       = "starter_small"
+  public_endpoint = true 
+}
