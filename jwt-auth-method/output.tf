636ef0bbdb5520319f921df04afb26e0759be782
vagrant <vagrant@hellocloud-native-box>
2024-09-27 14:21:08 +0000

    edit output.tf files

--- jwt-auth-method/output.tf
+++ jwt-auth-method/output.tf
@@ -1,4 +1,4 @@
 output "role_name" {
   description = "Name of the auth role all vcs /modules"
-  value       = vault_jwt_auth_backend_role.admin-role.role_name
+  value       = vault_jwt_auth_backend_role.admin_role.role_name
 }
