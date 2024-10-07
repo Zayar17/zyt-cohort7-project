4f0592d34177bc94b2375a88bc3dc2f118839212
vagrant <vagrant@hellocloud-native-box>
2024-10-07 10:21:10 +0000

    deleted two blocks in main.tf and data.tf

--- create-vpc-day3/data.tf
+++ create-vpc-day3/data.tf
@@ -2,7 +2,7 @@ data "aws_availability_zones" "azs" {
     state = "available"
 }
 
-data "vault_aws_access_credentials" "creds" {
-  backend = "aws-master"
-  role    = "admin-access-role"
-}
+# data "vault_aws_access_credentials" "creds" {
+#   backend = "aws-master"
+#   role    = "admin-access-role"
+# }
